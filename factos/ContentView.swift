3082a9d93cae875f46c3175fa487d21e87a6b29f
AlexGoodD <alejandrochavarb@gmail.com>
2025-03-21 17:04:50 -0600

    Initial Commit

---
+++ factos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  factos
+//
+//  Created by Alejandro on 21/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ factos/factosApp.swift
@@ -0,0 +1,17 @@
+//
+//  factosApp.swift
+//  factos
+//
+//  Created by Alejandro on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct factosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
